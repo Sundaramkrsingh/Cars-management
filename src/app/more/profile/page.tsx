611fdb92a7fe72c0d6573b1b0bc9848433052f24
Tarun Kumar <tarun.tk@bezt.in>
2024-02-26 21:13:46 +0530

    added comment

--- src/app/more/profile/page.tsx
+++ src/app/more/profile/page.tsx
@@ -2,6 +2,9 @@ import { Suspense } from "react"
 import ProfileTab from "./components/profile-tab"
 
 export default async function Profile() {
+
+  // this is a test comment
+
   return (
     <div className="px-5 pt-2 text-black">
       <Suspense>
